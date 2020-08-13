062a0ec0ee0f9d7e933fb408f000a5f062681f84
krzysztof <misiek.nalepa@wp.pl>
2020-08-13 13:07:08 +0200

    mock update

--- src/main/java/pl/kraleppa/model/dictionary/Console.java
+++ src/main/java/pl/kraleppa/model/dictionary/Console.java
@@ -3,8 +3,5 @@ package pl.kraleppa.model.dictionary;
 public enum Console {
     PLAYSTATION_4,
     XBOX_ONE,
-    NINTENDO_SWITCH,
-    PC,
-    PLAYSTATION_3,
-    XBOX_360
+    NINTENDO_SWITCH
 }
--- src/main/java/pl/kraleppa/repository/MockData.java
+++ src/main/java/pl/kraleppa/repository/MockData.java
@@ -18,21 +18,30 @@ public class MockData {
 
         for (int i = 0; i < 30; i++){
             gameRepository.save(new Game(
-                    "Butterfield 4",
+                    "Hejlo",
                     69.99,
                     Genre.SHOOTING,
-                    Console.XBOX_360,
+                    Console.XBOX_ONE,
                     "Fajna giera ogólnie polecam",
-                    "https://rockmetalshop.pl/pol_pl_plakat-BATTLEFIELD-4-COVER-151108_1.jpg"
+                    "https://s3.gaming-cdn.com/images/products/734/orig/halo-the-master-chief-collection-xbox-one-cover.jpg"
             ));
 
             gameRepository.save(new Game(
-                    "Assasyns krid",
+                    "The last of us",
                     169.99,
                     Genre.ACTION,
-                    Console.PC,
+                    Console.PLAYSTATION_4,
                     "Średnio fajna giera :(",
-                    "https://a.allegroimg.com/s1024/0c6119/fa1a54524aba9f95890c12a59c49"
+                    "https://a.allegroimg.com/s512/037b18/ced7818d4a9c914abafbd856762a/THE-LAST-OF-US-PART-2-II-PS4-PLAYSTATION-PL-DUBB"
+            ));
+
+            gameRepository.save(new Game(
+                    "Mario Kart",
+                    169.99,
+                    Genre.RACING,
+                    Console.NINTENDO_SWITCH,
+                    "Średnio fajna giera :(",
+                    "https://i0.wp.com/www.semperludo.com/wp-content/uploads/2017/04/Mario-Kart-8-Deluxe-Switch-cover.jpg?fit=456%2C738&ssl=1"
             ));
         }
     }
