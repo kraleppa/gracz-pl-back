700cd3c1a8437c8c9e3d2414d8490154431e30d8
krzysztof <misiek.nalepa@wp.pl>
2020-08-23 13:12:34 +0200

    setup fix

--- shell/setup.sh
+++ shell/setup.sh
@@ -3,6 +3,7 @@
 sudo apt-get update
 sudo apt install postgresql
 sudo -u postgres psql -c "CREATE DATABASE gracz_pl"
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'password'";
 sudo apt-get install git -y
 sudo apt-get install -yq maven
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
