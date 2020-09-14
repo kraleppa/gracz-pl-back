90fe87866c1335aba49f64630b49ec93eff1d88f
krzysztof <misiek.nalepa@wp.pl>
2020-09-14 14:50:20 +0200

    registration security fix

--- src/main/java/pl/kraleppa/config/SecurityConfig.java
+++ src/main/java/pl/kraleppa/config/SecurityConfig.java
@@ -29,7 +29,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().cors().disable().authorizeRequests()
-                .antMatchers("/authenticate").permitAll()
                 .antMatchers(HttpMethod.DELETE, "/api/v1/games/*").hasRole("ADMIN")
                 .antMatchers(HttpMethod.PUT, "/api/v1/games/*").hasRole("ADMIN")
                 .antMatchers(HttpMethod.POST, "/api/v1/games").hasRole("ADMIN")
